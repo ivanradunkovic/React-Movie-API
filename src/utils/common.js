a3594cb20ef9c5a0567f455cdfd433a62d29f8fd
Ivan <ivan1501@gmail.com>
2020-06-29 18:16:39 +0200

    Function for generating unique IDs.

---
+++ src/utils/common.js
@@ -0,0 +1,9 @@
+// A simple function for generation of unique IDs
+
+export const generateId = (function () {
+    let globalId = 0
+    return function () {
+        return globalId++
+    }
+})
+()
