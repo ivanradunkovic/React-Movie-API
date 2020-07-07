27d5499d71ad36109ad6620c5daa3e84132f0bf2
Ivan <ivan1501@gmail.com>
2020-07-07 14:52:54 +0200

    Wrong commit is fiex now.

--- src/components/MovieBrowser.js
+++ src/components/MovieBrowser.js
@@ -1,63 +1,75 @@
-import React from 'react'
+import React from "react"
 import LazyLoad from 'react-lazyload'
-import {connect} from "react-redux"
-import Typography from "@material-ui/core/Typography"
-import Container from "@material-ui/core/Container"
-import EmptyBlock from "./placeholders/EmptyBlock"
-import {fetchMovie} from "../store/domains/entities/entities.actions"
-import {getMovie} from "../store/utils"
-import {toggleFavorite} from "../store/domains/user/user.actions"
-import MovieCardFetch from "./MovieCardFetch"
+import PropTypes from 'prop-types'
+import MovieCard from "./MovieCard"
+import {makeStyles} from "@material-ui/core/styles"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import {Movie} from "./types/movie-type"
+import Button from "@material-ui/core/Button"
+import {Loop} from "@material-ui/icons"
+import MovieCardPlaceholder from "./placeholders/MovieCardPlaceholder"
 import MovieList, {MovieListItem} from "./MovieList"
 
-function Favorites({isAppReady, movieIds, entities, user, fetchMovie, toggleFavorite}) {
+const useStyles = makeStyles(theme => ({
+    loader: {
+        height: 400,
+        width: 300,
+        margin: "0 auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    button: {
+        marginTop: 20,
+    },
+    buttonProgress: {
+        color: theme.palette.primary,
+        animationDuration: '550ms',
+        marginRight: 6
+    },
+}))
+
+MovieBrowser.propTypes = {
+    movies: PropTypes.arrayOf(Movie),
+    isFetching: PropTypes.bool,
+    isFetched: PropTypes.bool,
+    totalMovies: PropTypes.number,
+    onLoadMore: PropTypes.func,
+    placeholdersAmount: PropTypes.number,
+}
+
+function MovieBrowser({movies = [], isFetching, isFetched, totalMovies, placeholdersAmount = 5, onLoadMore, onFavorite}) {
+    const classes = useStyles()
+
+    const canLoadMore = movies.length < totalMovies && !!onLoadMore
 
     return (
-        <Container style={{margin: '20px auto'}}>
-            <Typography component="h2" variant="h4" style={{margin: '40px 0'}}>
-                Favorite movies
-            </Typography>
-            {!movieIds.length && (
-                <EmptyBlock text="You haven't marked favorite movies yet"/>
-            )}
-            {!!movieIds.length && (
-                <MovieList>
-                    {movieIds.map(id => {
-                        const movie = getMovie(id, entities, user)
-                        return (
-                            <LazyLoad minheight={400} key={id} once>
-                                <MovieListItem>
-                                    <MovieCardFetch
-                                        id={id}
-                                        movie={movie}
-                                        fetch={fetchMovie}
-                                        ready={isAppReady}
-                                        onFavorite={toggleFavorite}
-                                    />
-                                </MovieListItem>
-                            </LazyLoad>
-                        )
-                    })}
-                </MovieList>
-            )}
-        </Container>
+        <React.Fragment>
+            <MovieList>
+                {(isFetched ? movies : Array.from(new Array(placeholdersAmount))).map((movie, index) => (
+                    <MovieListItem key={movie ? movie.id : 'index:'+index}>
+                        {movie ?
+                            <LazyLoad height={400} once placeholder={<MovieCardPlaceholder />}>
+                                <MovieCard {...movie} onFavorite={onFavorite} />
+                            </LazyLoad> :
+                            <MovieCardPlaceholder/>}
+                    </MovieListItem>
+                ))}
+            </MovieList>
+            {canLoadMore &&
+            <Button className={classes.button} variant="outlined" fullWidth size={"large"} disabled={isFetching} onClick={onLoadMore}>
+                {isFetching ?
+                    <CircularProgress
+                        className={classes.buttonProgress}
+                        variant="indeterminate"
+                        disableShrink
+                        size={16}
+                        thickness={4}/> :
+                    <Loop className={classes.buttonProgress}/>}
+                {isFetching ? 'Loading...' : 'Load more'}
+            </Button>}
+        </React.Fragment>
     )
 }
 
-function mapStateToProps(state) {
-    return {
-        isAppReady: state.common.isAppReady,
-        movieIds: state.user.favoriteMovieIds,
-        entities: state.entities,
-        user: state.user
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        fetchMovie: (id) => dispatch(fetchMovie(id)),
-        toggleFavorite: (id) => dispatch(toggleFavorite(id))
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Favorites)
+export default MovieBrowser
